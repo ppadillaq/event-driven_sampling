c391ebfd2750834c4ce0f64696b692af757fcf92
ppadillaq <pedro.padilla@sunamp.com>
2023-06-30 20:02:52 +0100

    Adds plot_sample methods to print in main gui the time sampled signal.

--- nodes.py
+++ nodes.py
@@ -1,28 +1,49 @@
+import pandas as pd
+
 class SensorNodeIntegral():
     """
     Sensor node for event-driven sampling under integral criterion.
     Inspired by Miskowicz, M (2005) The Event-Triggered Integral Criterion for Sensor
     Sampling
     """
-    def __init__(self, mu, T, Tmax):
+    def __init__(self, axis, signal, mu, T, Tmax):
+        self.sensor_plot = axis
+        self.signal = signal
         self.x_sent = 0
-        self.mu - mu
+        self.mu = mu
         self.T = T
         self.Tmax = Tmax
+        self.k = []
+        self.x = []
+
+
+    def plot_sample(self):
+        # data = {'k': self.k,
+        #  'value': self.x
+        #  }  
+        data = {'value': self.x
+         }  
+        df = pd.DataFrame(data)
+        df.plot(kind='line', legend=True, ax=self.sensor_plot, color='r', marker='o', fontsize=10)
+
 
     
-    def sample(self, x):
-        self.T_sent += self.T
-        # To do: calculate integral
-        if abs(x - self.x_sent) >= self.mu:
-            self.broadcast(x)
-            self.x_sent = x
-            self.reset()
-
-        else:
-            if self.T_sent == self.Tmax:
-                self.broadcast(x)
-                self.reset()
+    def sample(self, n_samples):
+        T_sent = 0
+        for k in range(n_samples):
+            self.k.append(k)
+            self.x.append(self.signal.get_sample(self.T*k))
+            self.plot_sample()
+            T_sent += self.T
+            # To do: calculate integral
+            # if abs(self.x[k] - self.x_sent) >= self.mu:
+            #     self.broadcast(self.x[k])
+            #     self.x_sent = self.x[k]
+            #     self.reset()
+            # else:
+            #     if T_sent == self.Tmax:
+            #         self.broadcast(self.x[k])
+            #         self.reset()
 
 
 
